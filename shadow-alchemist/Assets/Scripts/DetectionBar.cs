bf8bb849b8d56b6af3de684592a240dc21048200
AdamSimpson1317 <98826882+AdamSimpson1317@users.noreply.github.com>
2024-07-28 20:58:26 +0100

    Shop UI complete

--- shadow-alchemist/Assets/Scripts/DetectionBar.cs
+++ shadow-alchemist/Assets/Scripts/DetectionBar.cs
@@ -24,6 +24,7 @@ public class DetectionBar : MonoBehaviour
         thresholdTimer = 10;
         isDetectionLowered = false;
         isCoroutineStarted = false;
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
